26d70afc70aa4abc921e7b05b98d1db8ca5532d7
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-07-10 21:35:43 +0200

    added item class and set up url swaps through start_requests method

--- google_scraper/items.py
+++ google_scraper/items.py
@@ -11,3 +11,5 @@ import scrapy
 class GoogleScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
--- google_scraper/spiders/scraper.py
+++ google_scraper/spiders/scraper.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from scrapy_splash import SplashRequest
+
+from google_scraper.items import GoogleScraperItem
+
 
 class ScraperSpider(scrapy.Spider):
     name = 'scraper'
+    rotate_user_agent = True
     allowed_domains = ['google.com']
-    start_urls = ['https://google.com']
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        words = ['Michael', 'Scrapy']
+        for x in range(len(words)):
+            yield SplashRequest(f'https://google.com/search?q={words[x]}&num=100')
